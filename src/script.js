b72d39eec0907d5d4b8017b7b04357d21e2068ad
kaua-psilva <kauapaulinoo2005@gmail.com>
2024-12-16 12:23:02 -0400

    Att JavaScript verificar valores no input

--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
-let inputEmail = document.getElementById("")
+let inputEmail = document.getElementById("input-email")
+let errorEmail = document.getElementById("input-error-email");
+let btnVerify = document.getElementById("login-btn")
+function verificarEmail(){
+    if(inputEmail.value === ''){ 
+        errorEmail.innerText = "Preencha o email."
+        console.log("Senha OK!")
+    }
+    else return;
+};
+let passwordInput = document.getElementById("input-password")
+let errorPassword = document.getElementById("input-error-password");
+
+function verificarSenha(){
+    if(passwordInput.value === ''){ 
+        errorPassword.innerHTML = "Preencha o email."
+    }
+    else{
+        console.log("Senha OK!")
+    }
+}
+btnVerify.addEventListener('click', () => {
+    verificarEmail();
+    verificarSenha();
+});
